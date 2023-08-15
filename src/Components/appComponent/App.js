b978d457e5cc46aa7ac5275e1ef436af74274697
omer117 <omer.asraf91@gmail.com>
2023-08-15 10:36:53 +0300

    starting project

---
+++ src/Components/appComponent/App.js
@@ -0,0 +1,13 @@
+import NavbarComponent from '../navbarComponent/navbarComponent';
+import './App.scss';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+<NavbarComponent/>
+    </>
+  );
+}
+
+export default App;
